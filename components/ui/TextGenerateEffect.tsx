74f1bdfadc88c76a4006ce59d3e609289958be84
Neil Javiya <41284894+neiljaviya@users.noreply.github.com>
2024-12-12 21:58:41 -0500

    Update TextGenerateEffect.tsx

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -24,7 +24,7 @@ export const TextGenerateEffect = ({
         filter: filter ? "blur(0px)" : "none",
       },
       {
-        duration: duration ? duration : 0.6,
+        duration: duration ? duration : 1,
         delay: stagger(0.2),
       }
     );
